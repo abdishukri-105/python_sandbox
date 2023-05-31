a632e80847f02972f98f9e8cfe52e64c9575e212
abdishukri <abdishukrimohamed105@gmail.com>
2023-05-31 21:39:13 +0300

    finished lists

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,31 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+
+# create list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['apple', 'orange', 'green','pears']
+
+# get values
+print(fruits[1])
+print(len(fruits))
+fruits.append('Mangoes')
+
+fruits.remove('green')
+
+# change values
+fruits[0] = 'bluebarries'
+
+# insert into position
+fruits.insert(2, 'banana')
+
+# remove with pop
+fruits.pop(2)
+
+fruits.reverse()
+
+fruits.sort()
+
+# reverse sort
+fruits.sort(reverse=True)
+
+print(fruits)
